20656cde8f770ee4079572cf4dd13734ada15aab
Pletenetsky Sergey <sheepgamestudio@gmail.com>
2026-08-16 19:43:33 +0300

    Initial Commit

---
+++ Data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Data
+//
+//  Created by Sergey Pletenetsky on 16.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Data/DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataApp.swift
+//  Data
+//
+//  Created by Sergey Pletenetsky on 16.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
